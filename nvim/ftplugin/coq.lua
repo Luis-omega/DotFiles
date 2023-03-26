9f292812b094cd94c03ad063a06580e7c4055cbc
Luis-omega <73986926+Luis-omega@users.noreply.github.com>
2023-03-25 18:10:53 -0600

    chcanged formatting funcition

--- nvim/ftplugin/coq.lua
+++ nvim/ftplugin/coq.lua
@@ -1,5 +1,4 @@
-
 function vim.g.CoqtailHighlight()
-  vim.cmd[[hi def CoqtailChecked guifg=white guibg=#008000]]
-  vim.cmd[[hi def CoqtailSent guifg=black guibg=#d24dff]]
+  vim.cmd [[hi def CoqtailChecked guifg=white guibg=#008000]]
+  vim.cmd [[hi def CoqtailSent guifg=black guibg=#d24dff]]
 end
--- nvim/ftplugin/lua.lua
+++ nvim/ftplugin/lua.lua
@@ -4,7 +4,7 @@ vim.api.nvim_create_autocmd({ "BufWritePre" },
   {
     pattern = "*.lua"
     ,
-    callback = function() vim.lsp.buf.formatting_sync(nil, 100) end
+    callback = function() vim.lsp.buf.format({ timeout_ms = 1000 }) end
     ,
     group = autocmd_group
   })
