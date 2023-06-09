9512ba5cce44ce1ef1458f9b0506f44451197864
Luis-omega <73986926+Luis-omega@users.noreply.github.com>
2023-06-08 19:09:17 -0600

    change of completion system

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
 require("nativeSettings").setMyDefaultSettings()
 require('packerStartup')
 require('lsp_config').setAll()
-require('cmp_config')
+-- require('cmp_config')
 require('keybinds').setAll()
 
 vim.g.purescript_unicode_conceal_enable = 1
