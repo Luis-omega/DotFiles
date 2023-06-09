3887033c9857f083fff0746d3ade82d66aa9e8ea
Luis Alberto Diaz Diaz <73986926+Luis-omega@users.noreply.github.com>
2023-06-09 13:29:17 -0600

    cmp to a non bug version

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,7 @@
 require("nativeSettings").setMyDefaultSettings()
 require('packerStartup')
 require('lsp_config').setAll()
--- require('cmp_config')
+require('cmp_config')
 require('keybinds').setAll()
 
-vim.g.coq_settings = { auto_start = 'shut-up' }
 vim.g.purescript_unicode_conceal_enable = 1
--- nvim/lua/packerStartup.lua
+++ nvim/lua/packerStartup.lua
@@ -29,27 +29,21 @@ return require('packer').startup(function()
   use 'https://gitlab.com/yorickpeterse/nvim-window.git'
 
   -- autocompletion and snippets
---   use {
---     'hrsh7th/nvim-cmp'
---     , requires = {
---     { 'neovim/nvim-lspconfig' }
---     ,
---     { 'hrsh7th/cmp-nvim-lsp' }
---     ,
---     { 'hrsh7th/cmp-buffer' }
---     ,
---     { 'hrsh7th/cmp-path' }
---     ,
---     { 'hrsh7th/cmp-cmdline' }
---     ,
---     { 'L3MON4D3/LuaSnip' }
---     ,
---     { 'saadparwaiz1/cmp_luasnip' }
---     ,
---     commit = "98d5d0583c461752a8e917d0ec6b98466083dd23"
---   },
---   }
-  use {'ms-jpq/coq_nvim', branch='coq'}
+  use {
+    'hrsh7th/nvim-cmp'
+    , requires = {
+    { 'neovim/nvim-lspconfig' }
+    , { 'hrsh7th/cmp-nvim-lsp' }
+  , { 'hrsh7th/cmp-buffer' }
+  , { 'hrsh7th/cmp-path' }
+  --, { 'hrsh7th/cmp-cmdline' }
+  --
+  , { 'L3MON4D3/LuaSnip' }
+    --, { 'saadparwaiz1/cmp_luasnip' }
+  },
+    commit = "777450fd0ae289463a14481673e26246b5e38bf2"
+  }
+
   -- Latex
   use 'KeitaNakamura/tex-conceal.vim'
 
