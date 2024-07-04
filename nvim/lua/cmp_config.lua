73983939e5fcbca527b8496b151f4967e81c823a
Luis Alberto Díaz Díaz <73986926+Luis-omega@users.noreply.github.com>
2024-07-04 08:25:43 -0600

    added full buffer completion

--- nvim/lua/cmp_config.lua
+++ nvim/lua/cmp_config.lua
@@ -59,7 +59,15 @@ cmp.setup({
 
   }),
   sources = cmp.config.sources({
-    { name = 'buffer' },
+    {
+      name = 'buffer'
+      ,
+      option = {
+        get_bufnrs = function()
+          return vim.api.nvim_list_bufs()
+        end
+      }
+    },
     { name = 'nvim_lsp' },
   }, {
   })
