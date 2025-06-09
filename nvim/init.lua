fe53dbf03316e668b0de7a686620bf9853a51993
Luis Alberto <73986926+Luis-omega@users.noreply.github.com>
2025-06-09 09:37:06 -0600

    renamed diagnostics

--- nvim/init.lua
+++ nvim/init.lua
@@ -30,7 +30,7 @@ setAll("nativeSettings")
 require('packerStartup')
 setAll("colorScheme")
 setAll("indentation")
-setAll("lspSymbols")
+setAll("diagnostics")
 setAll("completion")
 setAll("treesitterConfiguration")
 
@@ -39,9 +39,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
-    -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
     if client:supports_method('textDocument/completion') then
-      -- Optional: trigger autocompletion on EVERY keypress. May be slow!
       local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
       client.server_capabilities.completionProvider.triggerCharacters = chars
       vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
